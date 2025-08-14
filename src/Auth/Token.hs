75baf53af8ce54138b0b4efc3a9e4e5c0665afe0
mrtstg <mrtstgdev@gmail.com>
2025-08-14 16:11:11 +0300

    feat: added instance for defining labforge-related services environment and generic functions for validation user token using auth service

--- src/Auth/Token.hs
+++ src/Auth/Token.hs
@@ -1,18 +1,56 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell     #-}
 module Auth.Token
   ( genericTokenFunctions
+  , lookupToken
+  , requireToken
+  , requireRealmRoles
+  , requireManyRealmRoles
   ) where
 
 import           Api.Keycloak.Models
+import qualified Api.Keycloak.Models.Introspect as I
 import           Api.Keycloak.Models.Token
 import           Api.Keycloak.Token
+import           Api.Keycloak.Utils
 import           Auth.Client
-import           Control.Monad.IO.Class
+import           Control.Monad.Except
 import           Control.Monad.Logger
+import           Control.Monad.Reader
+import           Data.Aeson                     (Value (..))
 import           Data.Text
+import           Models.JSONError
 import           Servant.Client
+import           Servant.Server
+import           Service.Environment
+
+lookupToken :: (MonadIO m, MonadLogger m, HasTokenVariable s Text, ServiceEnvironment s, MonadReader s m, MonadError ServerError m) => Text -> m I.IntrospectResponse
+lookupToken token = do
+  env <- asks $ getEnvFor AuthService
+  withTokenVariable'' $ \t -> (liftIO . flip runClientM env) (postValidateRequest token (BearerWrapper t))
+
+requireToken :: (MonadIO m, MonadLogger m, HasTokenVariable s Text, ServiceEnvironment s, MonadReader s m, MonadError ServerError m) => Text -> m I.IntrospectResponse
+requireToken token = do
+  r <- lookupToken token
+  case r of
+    I.InactiveToken -> sendJSONError err401 (JSONError "unauthorized" "Inactive token" Null)
+    d@(I.ActiveToken {}) -> pure d
+
+requireRealmRoles :: (MonadIO m, MonadLogger m, HasTokenVariable s Text, ServiceEnvironment s, MonadReader s m, MonadError ServerError m) => Text -> [Text] -> m I.IntrospectResponse
+requireRealmRoles token roles = requireManyRealmRoles token [roles]
+
+requireManyRealmRoles :: (MonadIO m, MonadLogger m, HasTokenVariable s Text, ServiceEnvironment s, MonadReader s m, MonadError ServerError m) => Text -> [[Text]] -> m I.IntrospectResponse
+requireManyRealmRoles token rolesMap = do
+  t <- requireToken token
+  case t of
+    ~(I.ActiveToken { .. }) -> do
+      if Prelude.any (Prelude.all (`Prelude.elem` tokenRealmRoles)) rolesMap then
+        pure t
+      else sendJSONError err403 (JSONError "unauthorized" "Insufficent permissions" Null)
 
 genericTokenFunctions :: (MonadLogger m, MonadIO m) => (Text, Text) -> ClientEnv -> TokenVariableFunctions Text m
 genericTokenFunctions (cID, cSecret) env = TokenFunctions {tokenValidateF=validate, tokenIssueF=issue} where
