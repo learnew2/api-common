45aeef328d1fa3cf3273fafaee7a8305a707048a
mrtstg <mrtstgdev@gmail.com>
2025-08-18 19:46:34 +0300

    fix: exported missing class deploymentstatus

--- src/Deployment/Models/Deployment.hs
+++ src/Deployment/Models/Deployment.hs
@@ -6,6 +6,7 @@ module Deployment.Models.Deployment
   , DeploymentInstance(..)
   , DeploymentInstanceBrief(..)
   , PowerState(..)
+  , DeploymentStatus(..)
   ) where
 
 import           Data.Aeson
