bcf8cb9c770fd57caf29895c17ddbafdb8ae0bea
mrtstg <mrtstgdev@gmail.com>
2025-08-19 11:18:05 +0300

    refactor: changed some of api types usage

--- src/Deployment/Schema.hs
+++ src/Deployment/Schema.hs
@@ -14,19 +14,18 @@ import           Deployment.Models.Deployment
 import           Proxmox.Deploy.Models.Config.Template
 import           Servant.API
 
-type TemplateNameCapture = Capture "TemplateName" Text
 type DeploymentTemplateCapture = Capture "DeploymentTemplateID" Int
 type NodeNameCapture = Capture "NodeName" Text
 
 type DeploymentAPI = "api" :> "deployment" :> "templates" :> QueryParam "page" Int :> AuthHeader :> Get '[JSON] (PagedResponse [ConfigTemplate])
-  :<|> "api" :> "deployment" :> "templates" :> TemplateNameCapture :> AuthHeader :> Delete '[JSON] ()
+  :<|> "api" :> "deployment" :> "templates" :> Capture "templateID" Int :> AuthHeader :> Delete '[JSON] ()
   :<|> "api" :> "deployment" :> "templates" :> ReqBody '[JSON] ConfigTemplate :> AuthHeader :> Post '[JSON] ()
   :<|> "api" :> "deployment" :> "deployments" :> QueryParam "page" Int :> AuthHeader :> Get '[JSON] (PagedResponse [DeploymentTemplate])
   :<|> "api" :> "deployment" :> "deployments" :> ReqBody '[JSON] DeploymentCreate :> AuthHeader :> Post '[JSON] ()
   :<|> "api" :> "deployment" :> "deployments" :> DeploymentTemplateCapture :> AuthHeader :> Get '[JSON] DeploymentTemplate
   :<|> "api" :> "deployment" :> "deployments" :> DeploymentTemplateCapture :> AuthHeader :> Delete '[JSON] ()
   :<|> "api" :> "deployment" :> "deployments" :> DeploymentTemplateCapture :> ReqBody '[JSON] DeploymentCreate :> AuthHeader :> Patch '[JSON] ()
-  :<|> "api" :> "deployment" :> "deployments" :> DeploymentTemplateCapture :> "deploy" :> "group" :> Capture "GroupName" Text :> AuthHeader :> Get '[JSON] ()
+  :<|> "api" :> "deployment" :> "deployments" :> DeploymentTemplateCapture :> "deploy" :> "group" :> QueryParam "group" Text :> AuthHeader :> Get '[JSON] ()
   :<|> "api" :> "deployment" :> "vmid" :> NodeNameCapture :> Capture "DeploymentInstanceID" Text :> QueryParam "amount" Int :> AuthHeader :> Get '[JSON] [Int]
   :<|> "api" :> "deployment" :> "display" :> NodeNameCapture :> Capture "DeploymentInstanceID" Text :> QueryParam "amount" Int :> AuthHeader :> Get '[JSON] [Int]
   :<|> "api" :> "deployment" :> "deployments" :> DeploymentTemplateCapture :> "instances" :> QueryParam "page" Int :> AuthHeader :> Get '[JSON] (PagedResponse [DeploymentInstance])
